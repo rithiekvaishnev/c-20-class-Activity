6acc6ca7e9756ae96567cc4ea63b10fc6b3f204a
rithiekvaishnev <74966777+rithiekvaishnev@users.noreply.github.com>
2020-12-03 17:56:23 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c-20-class-Activity
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,34 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(800,600);
+  fixedRect = createSprite(300, 300, 50, 80);
+  movingRect = createSprite(500,200,80,50);
+
+  fixedRect.debug = true;
+  movingRect.debug = true;
+
+  fixedRect.shapeColor = "green";
+  movingRect.shapeColor = "green";
+}
+
+function draw() {
+  background("black");  
+
+  fixedRect.shapeColor = "green";
+  movingRect.shapeColor = "green";
+
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+
+  if(movingRect.x - fixedRect.x <= fixedRect.width/2 + movingRect.width/2 && 
+    fixedRect.x - movingRect.x <= fixedRect.width/2 + movingRect.width/2 &&
+    movingRect.y - fixedRect.y <= fixedRect.height/2 + movingRect.height/2 && 
+    fixedRect.y - movingRect.y <= fixedRect.height/2 + movingRect.height/2){
+      fixedRect.shapeColor = "red";
+      movingRect.shapeColor = "red";
+
+  }
+
+  drawSprites();
+}
